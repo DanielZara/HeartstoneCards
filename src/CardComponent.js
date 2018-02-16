2e516e2f655912b829c16f5db9c4fc10f9bd2579
Daniel Zara <jekol7@o2.pl>
2018-02-16 17:44:20 +0100

    Changed source of tooltip data to one object called "tooltip"

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -5,17 +5,41 @@ import GImgComponent from "./GImgComponent";
 import './CardComponent.css';
 
 export default class CardComponent extends Component {
-    constructor(){
-        super();
-        this.state = {hasImg:true};
+    constructor(props){
+        super(props);
+        this.state = {
+            hasImg:true,
+            isToggleOn: false
+        };
+        this.handleEvent = this.handleEvent.bind(this);
     }
+
+    handleEvent() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    showGoldAndProps() { {/*Metoda wyświetlająca złotą wesję karty i informacje o niej (nie działa)*/}
+        if (this.state.isToggleOn) {
+            return (<div onMouseLeave={this.handleEvent} className="container">
+                <GImgComponent className="cards-component" onError={()=>this.onImgError()} imgGold={this.props.card.imgGold}/>
+                <DescriptionComponent className="descryption-component" card={this.props.card}/>
+                </div>
+            );
+        } else { return (
+            <div onMouseEnter={this.handleEvent} className="container-mouse-out">
+            <ImgComponent   className="cards-component" onError={()=>this.onImgError()} img={this.props.card.img}/>
+                <br/>
+            </div>);
+        }
+    }
+
     render() {
         if (this.props.card && this.state.hasImg)
             return (
-                <div className="container">
-                    <ImgComponent className="cards-component" onError={()=>this.onImgError()} img={this.props.card.img}/>
-                    <GImgComponent className="cards-component" onError={()=>this.onImgError()} imgGold={this.props.card.imgGold}/>
-                    <DescriptionComponent className="descryption-component" card={this.props.card}/>
+                <div>
+                    {this.showGoldAndProps()}
                 </div>
             );
         else return null;
